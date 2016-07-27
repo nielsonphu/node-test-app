a2a3fbcb28e720872e26663aa884bbaa77cd10a9
Nielson Phu <nsnphu@gmail.com>
2016-07-27 10:53:18 -0400

    Configure mongodb.

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
+
 var app = express();
 
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'))
 
+app.get('/api/jobs', function(req, res) {
+	mongoose.model('Job').find({}).exec(function(error, collection) {
+		res.send(collection);
+	});
+});
+
 app.get('*', function(req, res) {
 	res.render('index');
 });
+
+// mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://jobsearcher:jobsearcher@ds031925.mlab.com:31925/jobsearcher');
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+	console.log('connected to mongodb successfully');
+	jobModel.seedJobs();
+});
+
+console.log('port', process.env.PORT);
+console.log('IP', process.env.IP);
 
 app.listen(process.env.PORT, process.env.IP);
